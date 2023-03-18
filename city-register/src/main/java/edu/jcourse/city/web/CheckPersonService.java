71bf9af5eea66168423893153ff4035092e8cd1e
Anton Davydovskyi <anton.davydoone@mail.ru>
2023-03-18 15:54:43 +0300

    refactoring: create ReST Service singleton

--- city-register/src/main/java/edu/jcourse/city/web/CheckPersonService.java
+++ city-register/src/main/java/edu/jcourse/city/web/CheckPersonService.java
@@ -1,7 +1,13 @@
 package edu.jcourse.city.web;
 
+import edu.jcourse.city.dao.DAOProvider;
+import edu.jcourse.city.dao.PersonCheckDAO;
+import edu.jcourse.city.dao.impl.PoolConnectionBuilder;
 import edu.jcourse.city.domain.PersonRequest;
 import edu.jcourse.city.domain.PersonResponse;
+import edu.jcourse.city.exception.PersonCheckDAOException;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -11,15 +17,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Path("/check")
+@Singleton
 public class CheckPersonService {
 
     private static final Logger logger = LoggerFactory.getLogger(CheckPersonService.class);
+    private PersonCheckDAO dao;
+
+    @PostConstruct
+    public void init() {
+        logger.info("START");
+        dao = DAOProvider.getInstance().getPersonCheckDAO();
+        dao.setConnectionBuilder(new PoolConnectionBuilder());
+    }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public PersonResponse checkPerson(PersonRequest request) {
+    public PersonResponse checkPerson(PersonRequest request) throws PersonCheckDAOException {
         logger.info("{}", request);
-        return new PersonResponse();
+        return dao.checkPerson(request);
     }
 }
