95e2984405cc541678317af7efd695623f9053b2
Anton Davydovskyi <anton.davydoone@mail.ru>
2023-03-20 15:34:06 +0300

    create domain

---
+++ register-office/src/main/java/edu/jcourse/register/domain/Person.java
@@ -0,0 +1,41 @@
+package edu.jcourse.register.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
+    private Long personId;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
