1e646adca7209a695d3f4fbf86979a60a18729fc
Anton Davydovskyi <anton.davydoone@mail.ru>
2023-03-20 19:22:55 +0300

    use JPA instead Hibernate

--- register-office/src/main/java/edu/jcourse/register/manager/PersonManager.java
+++ register-office/src/main/java/edu/jcourse/register/manager/PersonManager.java
@@ -1,6 +1,7 @@
 package edu.jcourse.register.manager;
 
 import edu.jcourse.register.domain.Person;
+import jakarta.persistence.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -13,6 +14,37 @@ import org.hibernate.query.Query;
 public class PersonManager {
 
     public static void main(String[] args) {
+//        sessionExample();
+
+        jpaExample();
+    }
+
+    private static void jpaExample() {
+        EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("persistence");
+
+        EntityManager entityManager = managerFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Person person = new Person();
+        person.setFirstName("Алексей");
+        person.setLastName("Федоров");
+
+        entityManager.persist(person);
+        System.out.println(person.getPersonId());
+
+        transaction.commit();
+        entityManager.close();
+
+        entityManager = managerFactory.createEntityManager();
+        TypedQuery<Person> from_person = entityManager.createQuery("FROM Person", Person.class);
+        from_person.getResultList().forEach(System.out::println);
+
+        entityManager.close();
+    }
+
+    private static void sessionExample() {
         SessionFactory sessionFactory = buildSessionFactory();
 
         Session session = sessionFactory.openSession();
@@ -29,16 +61,15 @@ public class PersonManager {
         transaction.commit();
         session.close();
 
-        Session session1 = sessionFactory.openSession();
-        Person person1 = session1.get(Person.class, id);
+        session = sessionFactory.openSession();
+        Person person1 = session.get(Person.class, id);
         System.out.println(person1);
-        session1.close();
+        session.close();
 
         session = sessionFactory.openSession();
         Query<Person> from_person = session.createQuery("FROM Person", Person.class);
 
-
-        System.out.println(from_person.list());
+        from_person.list().forEach(System.out::println);
         session.close();
     }
 
